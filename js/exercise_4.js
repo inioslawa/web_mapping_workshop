65244c3e5cfe10fb80f0f3cfa80e81e9f322432e
Inia <jajezior@ncsu.edu>
2016-05-02 14:35:18 -0400

    token and ID

--- js/exercise_4.js
+++ js/exercise_4.js
@@ -2,10 +2,11 @@
 
 // Enter your mapbox map id here to reference it for the base layer,
 // this one references the ugly green map that I made.
-var mapId = 'will-breitkreutz.k6fj4l3f';
+var mapId = 'inioslawa.019h7dhi';
 
 // And this is my access token, use yours.
-var accessToken = 'pk.eyJ1Ijoid2lsbC1icmVpdGtyZXV0eiIsImEiOiItMTJGWEF3In0.HEvuRMMVxBVR5-oDYvudxw';
+var accessToken = 'pk.eyJ1IjoiaW5pb3NsYXdhIiwiYSI6ImNpbnFidXU1djEwM2h1a2x5N3hycDF4ZTIifQ.cAJsoGJ9BTfSalIjlugqkw
+';
 
 // Create the map object with your mapId and token,
 // referencing the DOM element where you want the map to go.
